20442c19b931d5b6b2fb270953eca8e540926221
RuslanKozin <ruslan3723@mail.ru>
2016-08-31 19:14:24 +0300

    правка

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -29,7 +29,7 @@ class AppAsset extends AssetBundle
     public $js = [
      //   'js/jquery.js',
      //   'js/bootstrap.min.js',
-        'js/jquery.scr',
+        'js/jquery.js',
         'js/price-range.js',
         'js/jquery.prettyPhoto.js',
         'js/main.js',
