fde4165f64685e3f5961cff260494118606a354a
RuslanKozin <ruslan3723@mail.ru>
2017-03-12 15:54:59 +0300

    Внес комментарий

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -36,7 +36,7 @@ class AppAsset extends AssetBundle
         'js/jquery.accordion.js',
         'js/main.js',
     ];
-    public $depends = [                 //Зависимости скриптов и стилей
+    public $depends = [                 //Зависимости скриптов и стилей новые
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapPluginAsset', /*Эта зависимость заменяет подключ. файлы выше
                                                 (css/bootstrap.min.css, js/jquery.js, js/bootstrap.min.js)*/
