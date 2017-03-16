f60e331fcc49351e4c8589059e17a1bae64f21b2
RuslanKozin <ruslan3723@mail.ru>
2017-03-16 20:02:45 +0300

    Добавил комментарии

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -8,26 +8,27 @@ use yii\base\Widget;
 
 class MenuWidget extends Widget
 {
-    public $tpl;
+    public $tpl;        //параметр для виджета категорий, которое выдает меню состоящее из (ul и li)
     public $data;       //Свойство в котором будет хранится все данные категорий из базы данных(массив)
     public $tree;       //Результат работы $date. (строит массив дерева категории по подгруппам)
     public $menuHtml;   //Готовый html-код меню
 
     public function init()  //Метод
     {
-        parent::init(); //Вызываем родительский метод
+        parent::init();     //Вызываем родительский метод
+        
             //Смотрим, что пришло в качестве параметра
         if ($this->tpl === null)    /*Если данный параметр не передан, т.е. tpl пустой*/
         {
             $this->tpl = 'menu';    //тогда присваиваем по умолчанию
         }
-        $this->tpl .= '.php';  //прикрепляем расширение
+        $this->tpl .= '.php';  //прикрепляем расширение шаблона
     }
 
-    public function run()
+    public function run()       //Метод run чаще всего используется для вывода данных
     {
         $this->data = Category::find()->indexBy('id')->asArray()->all();   /*Метод asArray - вернет результат ввиде массива-массива
-            Метод indexBy - какое поле/колонку таблицы использовать для индексирования массивов(ключи совпадают с id)*/
+            Метод indexBy - позволяет указать какое поле/колонку таблицы использовать для индексирования массивов(ключи совпадают с id)*/
         $this->tree = $this->getTree();  //Строем дерево
         $this->menuHtml = $this->getMenuHtml($this->tree);      //Строем html-код
         return $this->menuHtml;  //Выводим, что попало в tpl
@@ -49,16 +50,16 @@ class MenuWidget extends Widget
 
     protected function getMenuHtml($tree)   /*Метод getMenuHtml принимает в себя параметр, в нашем случае дерево, */
     {
-        $str = '';
+        $str = '';      // в данную переменную будет помешаться готовый html-код
         foreach ($tree as $category) {
             $str .= $this->catToTemplate($category);
         }
-        return $str;
+        return $str;    /* В итоге пройдясь в цикле по всему дереву формируем нужный html-код, который возвращает метод getMenuHtml  */
     }
 
-    protected function catToTemplate()
+    protected function catToTemplate()      //Метод catToTemplate возвращает буферизированный вывод в $str
     {
-        ob_start();
+        ob_start();         //Фукнция ob_start буферизирует вывод, а затем его возвращает не выводя при этом на экран и возвращает в $str
         include __DIR__ . '/menu_tpl/' . $this->tpl;
         return ob_get_clean();
     }
