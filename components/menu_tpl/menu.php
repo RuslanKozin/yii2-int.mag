f37fcd30a7618c860298b3f6094f9c9e7a0c1a06
RuslanKozin <ruslan3723@mail.ru>
2017-03-16 20:02:04 +0300

    Добавил комментарий

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -5,7 +5,7 @@
             <span class="badge pull-right"><i class="fa fa-plus"></i></span>
         <?php endif; ?>
     </a>
-    <?php if (isset($category['childs'])): ?>
+    <?php if (isset($category['childs'])):  //Если существуют потомки ?>
         <ul>
             <?= $this->getMenuHtml($category['childs']) ?>
         </ul>
