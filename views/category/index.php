c5e1e7dc3a4026f5ba6f6055a4883c164aaa2900
RuslanKozin <ruslan3723@mail.ru>
2017-03-19 19:03:23 +0300

    Добавил хелпер для изображений

--- views/category/index.php
+++ views/category/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'My Yii Application';
 ?>
 <section id="slider"><!--slider-->
@@ -118,18 +120,26 @@ $this->title = 'My Yii Application';
                                     <div class="product-image-wrapper">
                                         <div class="single-products">
                                             <div class="productinfo text-center">
-                                                <img src="images/home/product1.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
+                                                <?= Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name]) ?>
+                                                <h2>$<?= $hit->price ?></h2>
+                                                <p><?= $hit->name ?></p>
                                                 <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                             </div>
                                             <div class="product-overlay">
                                                 <div class="overlay-content">
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
+                                                    <h2>$<?= $hit->price ?></h2>
+                                                    <p><?= $hit->name ?></p>
                                                     <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                                 </div>
                                             </div>
+                                            <?php if($hit->new): ?>
+                                                <?= Html::img("@web/images/home/new.png", ['alt' => 'Новинка', 'class' => 'new']) ?>
+                                            <?php endif; ?>
+
+                                            <?php if($hit->sale): ?>
+                                                <?= Html::img("@web/images/home/sale.png", ['alt' => 'Распродажа', 'class' => 'new']) ?>
+                                            <?php endif; ?>
+
                                         </div>
                                         <div class="choose">
                                             <ul class="nav nav-pills nav-justified">
