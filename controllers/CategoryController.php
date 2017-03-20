4372f65b7b2beaca9fa9aa8c102d608fac5c33ec
RuslanKozin <ruslan3723@mail.ru>
2017-03-20 15:55:19 +0300

    Добавил метод для вывода метатегов В файл AppController.php добавил метод setMenu В файл CategoryController.php добавил код для вывода метатегов на страницу

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -10,13 +10,15 @@ class CategoryController extends AppController {
 
     public function actionIndex() {     //Действие по умолчанию (для данной таблицы)
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
-
+        $this->setMeta('E-SHOPPER');
         return $this->render('index', compact('hits'));
     }
 
     public function actionView($id) {
         $id = Yii::$app->request->get('id');        //Получаем id категории
         $products = Product::find()->where(['category_id' => $id])->all();    //Получаем продукты по данному id
+        $category = Category::findOne($id);         //Получаем id одной категории
+        $this->setMeta('E-SHOPPER | ' . $category->name, $category->keywords, $category->description);
         return $this->render('view', compact('products'));
     }
 
